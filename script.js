788fc779fefb7a9f1364467fe0843d9379bdbbbe
aminiporshyd-ai <aminiporshyd@gmail.com>
2026-08-12 21:18:09 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const correctPassword = "14041214";
 
+const passwordInput = document.getElementById("passwordInput");
+const enterButton = document.getElementById("enterButton");
+const errorMessage = document.getElementById("errorMessage");
+
+enterButton.addEventListener("click", function () {
+    const enteredPassword = passwordInput.value;
+
+    if (enteredPassword === correctPassword) {
+        errorMessage.textContent = "تم فتح الهدية ❤️";
+
+        setTimeout(function () {
+            alert("هله هله اب احله قمرررر ❤️");
+        }, 300);
+    } else {
+        errorMessage.textContent = "الرمز غير صحيح، حاولي مرة أخرى ";
+        passwordInput.value = "";
+        passwordInput.focus();
+    }
+});
+
+passwordInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        enterButton.click();
+    }
+});
